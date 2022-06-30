7f0c487956ea5f72d49f536023f908fda6369bdb
Willie Chalmers III <willie.chalmers@utdallas.edu>
2022-06-30 06:35:22 -0500

    chore: Start work on image stories

---
+++ components/wrapped/ImageStoryPage.tsx
@@ -0,0 +1,7 @@
+export default function ImageMemoryPage() {
+  return (
+    <div className="w-full p-8 rounded-b-[16px] lg:rounded-t-[16px] bg-secondary text-center">
+      <div>{/* TODO */}</div>
+    </div>
+  );
+}
