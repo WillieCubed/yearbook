154a64bf16d8f59d7298fdfc93609ee04e44c683
Willie Chalmers III <willie.chalmers@utdallas.edu>
2022-06-30 06:34:08 -0500

    chore: Add color scheme to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
         "Oxygen", "Ubuntu", "Helvetica Neue", "Arial", "sans-serif", "Apple Color Emoji",
         "Segoe UI Emoji", "Segoe UI Symbol"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9BD1E5',
+        secondary: '#A99FD1',
+        extra1: '#84E296',
+        extra2: '#F49F0A',
+        extra3: '#E873CB',
+      },
+    },
   },
   plugins: [],
 };
