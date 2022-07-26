d3d1e4a78562c25eb6d113568fa1fb6a63107a82
Willie Chalmers III <willie.chalmers@utdallas.edu>
2022-07-25 23:10:28 -0500

    chore: Update site footer styles
    Now there are some additional tweaks for hover/focus.

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -3,10 +3,16 @@
  */
 export default function SiteFooter() {
   return (
-    <footer className="bottom-0 relative p-4 text-center font-bold bg-slate-200 dark:bg-slate-800">
-      Built with ❤️ by{" "}
+    <footer className="bottom-0 relative p-4 text-center font-bold font-display bg-slate-200 dark:bg-slate-800">
       <a
-        className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+        className="text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
+        href="https://github.com/WillieCubed/clark-dashboard"
+      >
+        Built
+      </a>{" "}
+      with ❤️ by{" "}
+      <a
+        className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400 focus:text-blue-600 dark:focus:text-blue-400 transition ease-in-out duration-200"
         href="https://williecubed.me"
       >
         Willie Chalmers III
