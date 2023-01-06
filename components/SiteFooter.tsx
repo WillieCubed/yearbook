12bb8154731a992da06b502a3df814ff55e378b5
Willie Chalmers III <willie@williecubed.me>
2023-01-06 00:47:10 -0600

    feat: Update styles in footer to be more accessible

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -3,16 +3,16 @@
  */
 export default function SiteFooter() {
   return (
-    <footer className="bottom-0 relative p-3 text-center font-bold font-display bg-slate-200 dark:bg-slate-800">
+    <footer className="border-t-4 border-black bottom-0 relative p-3 text-center font-bold font-display bg-slate-200 dark:bg-slate-800">
       <a
-        className="text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
+        className="text-blue-500 dark:text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
         href="https://github.com/WillieCubed/yearbook"
       >
         Built
       </a>{" "}
       with ❤️ by{" "}
       <a
-        className="text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
+        className="text-blue-500 dark:text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
         href="https://williecubed.me"
       >
         Willie Chalmers III
