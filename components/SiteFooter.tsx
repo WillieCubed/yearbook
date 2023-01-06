9dc07182b732cc3e391556f816424e4ef7c53224
Willie Chalmers III <willie@williecubed.me>
2023-01-05 22:18:46 -0600

    Update site footer styles

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -3,7 +3,7 @@
  */
 export default function SiteFooter() {
   return (
-    <footer className="bottom-0 relative p-4 text-center font-bold font-display bg-slate-200 dark:bg-slate-800">
+    <footer className="bottom-0 relative p-3 text-center font-bold font-display bg-slate-200 dark:bg-slate-800">
       <a
         className="text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
         href="https://github.com/WillieCubed/yearbook"
@@ -12,7 +12,7 @@ export default function SiteFooter() {
       </a>{" "}
       with ❤️ by{" "}
       <a
-        className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400 focus:text-blue-600 dark:focus:text-blue-400 transition ease-in-out duration-200"
+        className="text-primary hover:text-blue-600 dark:hover:text-blue-200 focus:text-blue-600 dark:focus:text-blue-200 transition ease-in-out duration-200"
         href="https://williecubed.me"
       >
         Willie Chalmers III
