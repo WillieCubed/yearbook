414260247018464e680f6fdec35aad5d83ac3a3f
Willie Chalmers III <willie@williecubed.me>
2023-01-06 18:42:08 -0600

    feat: Add social meta tags to site
    Featuring a shiny new og:image!

--- app/(user)/head.tsx
+++ app/(user)/head.tsx
@@ -1,4 +1,8 @@
 export default function Head({ params }: { params: { slug: string } }) {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_VERCEL_URL ||
+    process.env.DEFAULT_URL ||
+    "https://utdwrapped.vercel.app";
   return (
     <>
       <title>Yearbook</title>
@@ -6,10 +10,16 @@ export default function Head({ params }: { params: { slug: string } }) {
         name="description"
         content="Review what happened in your past year at UTD."
       />
+
+      <meta property="og:title" content="Year in UTD" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={baseUrl} />
+      <meta property="og:image" content={`${baseUrl}/assets/meta/index.png`} />
       <meta
-        name="og:description"
+        property="og:description"
         content="Review what happened in your past year at UTD."
       />
+      <meta property="og:site_name" content="Year in UTD" />
     </>
   );
 }
