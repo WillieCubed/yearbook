725af0a99c86b796a1a3d987f918fd089af8861b
Willie Chalmers III <willie@williecubed.me>
2023-01-06 21:19:41 -0600

    chore: Make more mobile-responsive changes
    Woo responsiveness FTW

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -8,18 +8,18 @@ import SiteFooter from "../../components/SiteFooter";
 export default function LandingPage() {
   return (
     <div className="h-screen">
-      <main className="h-full p-8 bg-[#3BCEAC] dark:bg-[#540D6E]">
+      <main className="h-full p-4 md:p-8 bg-[#3BCEAC] dark:bg-[#540D6E]">
         <section className="mx-auto max-w-4xl">
-          <div className="mt-16 lg:mt-32 p-8 lg:p-16 border-4 border-black text-[#333333] bg-[#FFD23F] dark:text-white dark:bg-[#2767CE] drop-shadow-pop">
-            <div className="font-display font-bold text-4xl lg:text-7xl">
+          <div className="mt-16 md:mt-32 p-8 md:p-16 border-4 border-black text-[#333333] bg-[#FFD23F] dark:text-white dark:bg-[#2767CE] drop-shadow-pop">
+            <div className="font-display font-bold text-5xl md:text-7xl">
               Your UTD in review.
             </div>
-            <div className="mt-4 lg:mt-8 font-display font-bold lg:text-5xl">
+            <div className="mt-4 md:mt-8 font-display font-bold text-3xl md:text-5xl">
               Coming this May.
             </div>
           </div>
           <a
-            className="mt-[72px] lg:mt-[144px] inline-block p-6 border-4 border-black text-[#333333] bg-[#EE4266] dark:text-white dark:bg-[#3BCEAC] drop-shadow-pop hover:-translate-x-[8px] hover:-translate-y-[8px] hover:drop-shadow-pop-1 focus:-translate-x-[8px] focus:-translate-y-[8px] focus:drop-shadow-pop-1 font-display font-semibold text-3xl hover:cursor-pointer hover:text-blue-400 underline transition ease-in-out duration-200"
+            className="mt-[72px] md:mt-[144px] inline-block p-4 md:p-6 border-4 border-black text-[#333333] bg-[#EE4266] dark:text-white dark:bg-[#3BCEAC] drop-shadow-pop hover:-translate-x-[8px] hover:-translate-y-[8px] hover:drop-shadow-pop-1 focus:-translate-x-[8px] focus:-translate-y-[8px] focus:drop-shadow-pop-1 font-display font-semibold text-2xl md:text-3xl hover:cursor-pointer hover:text-blue-400 underline transition ease-in-out duration-200"
             title="Ha, did you really think I was that far along in this project?"
             href={process.env.WAITLIST_FORM}
           >
